e721668cbe0b38d9d8fe42f241c0315ad468727a
Joshua Jipp <102313118+joshuajipp@users.noreply.github.com>
2024-02-07 12:40:33 -0700

    aws config

--- infra/itemService.tf
+++ infra/itemService.tf
@@ -6,4 +6,14 @@ terraform {
       name = "itemService"
     }
   }
+  required_providers {
+    aws = {
+      version = ">= 4.0.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ca-central-1"
 }
