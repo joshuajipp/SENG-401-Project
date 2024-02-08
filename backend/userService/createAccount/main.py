a9d9dda9f22ac0555704d98b0a2ee08c3126fb45
HoussemZ4 <114112576+HoussemZ4@users.noreply.github.com>
2024-02-08 15:18:09 -0700

    Added createUser lambda function

--- backend/userService/createAccount/main.py
+++ backend/userService/createAccount/main.py
@@ -1,2 +1,33 @@
+import json
+import boto3
+import uuid
+
+dynamodb_resource = boto3.resource("dynamodb")
+table = dynamodb_resource.Table("users_db") # change name 
+
 def handler(event, context):
-  return {}
+    data = json.loads(event["body"])
+    try:
+        item={
+            "id": str(uuid.uuid4()),
+            "name": data["name"],
+            "email": data["email"],
+            "rating": data["rating"],
+            "bio": data["bio"],
+            "location": data["location"]
+            }
+        table.put_item(Item=item)
+        return {
+            "statusCode": 200,
+                "body": json.dumps(item)
+        }
+    except Exception as e:
+        print(f"Exception: {e}")
+        return {
+            "statusCode": 500,
+                "body": json.dumps({
+                    "message": str(e)
+                })}
+
+
+
