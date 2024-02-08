74d8fc651e369f32e370d900a786ed5b74322766
Joshua Jipp <102313118+joshuajipp@users.noreply.github.com>
2024-02-08 11:27:33 -0700

    test change

--- backend/itemService/createItem/main.py
+++ backend/itemService/createItem/main.py
@@ -3,6 +3,7 @@ import json
 
 def handler(event, context):
     response = requests.get('https://httpbin.org/get')
+    print("hi")
     return {
         'statusCode': 200,
         'body': json.dumps(response.json())
