fd6275f8e951f0acfaa99ee3f43ff40a72cdedde
Dominic Choi <69835046+CarrotBRRR@users.noreply.github.com>
2024-02-14 10:29:06 -0700

    Fixing createItem tests (#17)
    * Updated createItem.py
    
    * Naive implementation of main.py for createItem and updateItem
    
    * Changed UpdateExpression batch update dynamodb table entry
    
    * Use sha256 to compare new image to old image, Added Hash to table upload
    
    * Started test_create_item.py
    
    * Testing using test_create_item.py
    
    * Fixed tests not running (bad indentations)

--- backend/itemService/createItem/test_create_item.py
+++ backend/itemService/createItem/test_create_item.py
@@ -1,4 +1,5 @@
 import pytest
+import pytest
 from main import *
 from moto import mock_aws
 import boto3
@@ -17,39 +18,41 @@ def dynamodb_mock(aws_credentials):
     with mock_aws():
         yield boto3.resource('dynamodb', region_name='ca-central-1')
 
+def test_create_item(dynamodb_mock):
+    table_name = 'items-30144999'
+    dynamodb_mock.create_table(
+        TableName=table_name,
+        KeySchema=[{'AttributeName': 'itemID', 'KeyType': 'HASH'}],
+        AttributeDefinitions=[{'AttributeName': 'itemID', 'AttributeType': 'S'}],
+        ProvisionedThroughput={'ReadCapacityUnits': 1, 'WriteCapacityUnits': 1}
+    )
 
-        def test_create_item(dynamodb_mock):
-            # Arrange
-            table_name = 'items-30144999'
-            mock_data = {
-                'lenderID': "Len Derr",
-                'itemName': "Eye Temm",
-                'itemID': '1234',
-                'description': "a description",
-                'maxBorrowDays': 69,
-                'image': "url.com",
-                'imageHash': "HAHAHASH"
-            }
+    table = dynamodb_mock.Table(table_name)
 
-            dynamodb_mock.create_table(
-                TableName=table_name,
-                KeySchema=[{'AttributeName': 'itemID', 'KeyType': 'HASH'}],
-                AttributeDefinitions=[{'AttributeName': 'itemID', 'AttributeType': 'S'}],
-                ProvisionedThroughput={'ReadCapacityUnits': 1, 'WriteCapacityUnits': 1}
-            )       
+    mock_item = {
+        'lenderID': "Len Derr",
+        'itemName': "Eye Temm",
+        'itemID': '1234',
+        'description': "a description",
+        'maxBorrowDays': 69,
+        'image': "url.com",
+        'imageHash': "HAHAHASH"
+    }
 
-            response = insert_item_in_table(table_name, mock_data, dynamodb_mock)
+    insertion = insert_item_in_table(table_name, mock_item, dynamodb_mock)
 
-            # Assert
-            table = dynamodb_mock.Table(table_name)
-            response = table.get_item(Key={'id': '1234'})
+    # Assert
+    table = dynamodb_mock.Table(table_name)
+    response = table.get_item(Key={'id': '1234'})
 
-            assert 'Item' in response
-            assert response['Item']['itemID'] == '1234'
-            assert response['Item']['lenderID'] == 'Len Derr'
-            assert response['Item']['itemName'] == 'Eye Temm'
-            assert response['Item']['description'] == 'a description'
-            assert response['Item']['maxBorrowDays'] == 69
-            assert response['Item']['image'] == 'url.com'
-            assert response['Item']['imageHash'] == 'HAHAHASH'
+    assert insertion == 'CREATED_NEW'
+
+    assert 'Item' in response
+    assert response['Item']['itemID'] == '1234'
+    assert response['Item']['lenderID'] == 'Len Derr'
+    assert response['Item']['itemName'] == 'Eye Temm'
+    assert response['Item']['description'] == 'a description'
+    assert response['Item']['maxBorrowDays'] == 69
+    assert response['Item']['image'] == 'url.com'
+    assert response['Item']['imageHash'] == 'HAHAHASH'
 
