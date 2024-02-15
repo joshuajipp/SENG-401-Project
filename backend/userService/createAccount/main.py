ff37aaa98d5f1b35ab65c958490fa7911a32fa4e
HoussemZ4 <114112576+HoussemZ4@users.noreply.github.com>
2024-02-15 14:22:45 -0700

    Create account (#14)
    * changed user db variable names
    
    * changed database name in createUser

--- backend/userService/createAccount/main.py
+++ backend/userService/createAccount/main.py
@@ -3,14 +3,14 @@ import boto3
 import uuid
 
 dynamodb_resource = boto3.resource("dynamodb")
-table = dynamodb_resource.Table("users_db") # change name 
+table = dynamodb_resource.Table("users_dynamodb_table")  
 
 def handler(event, context):
     data = json.loads(event["body"])
     try:
         item={
-            "id": str(uuid.uuid4()),
-            "name": data["name"],
+            "userID": str(uuid.uuid4()),
+            "userName": data["name"],
             "email": data["email"],
             "rating": data["rating"],
             "bio": data["bio"],
