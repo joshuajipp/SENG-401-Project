a8777db388001009018ddf0e3ca231371c298329
MacKante <114007326+MacKante@users.noreply.github.com>
2024-02-23 15:19:26 -0700

    testing delete account (#33)

--- backend/userService/deleteAccount/main.py
+++ backend/userService/deleteAccount/main.py
@@ -1,25 +1,37 @@
 import json
 import boto3
 
-def handler(event, context):      
-  try:
-    # setup connection to dynamodb table
-    # TODO: remember to switch the name of table
+# Initialize a DynamoDB resource and get the table.
+def get_dynamodb_table(table_name):
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('name of table here!!!')
-    
-    # Read json and find userID
-    user_data = json.loads(event['body'])
-    userID = user_data['userID']
-    
-    # Find the account entry on table and delete it
-    # Return the account entry afterwards
-    response = table.delete_item(Key={'userID': userID})
-    return {
-      'statuscode': 200,
-      'body': json.dumps(response['Attributes'])
-    }
-    
+    table = dynamodb.Table(table_name)
+    return table
+
+# Remove an item from the DynamoDB table.
+def delete_account(table, userID):
+    response = table.delete_item(
+        Key={
+            'userID': userID
+        }
+    )
+    return response
+  
+
+def handler(event, context):
+  try:
+      # Retrieve table
+      table_name = 'users-30144999'
+      table = get_dynamodb_table(table_name)
+      
+      # Retrieve userID and attempt to delete account
+      userID = event['header']['userID']
+      response = delete_account(table, userID)
+      
+      return {
+          'statusCode': 200,
+          'body': json.dumps(response)
+      }
+      
   # Something went wrong with the delete  
   except Exception as e:
     print(f"exception: {e}")
