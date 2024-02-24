97078eeca17fbd04fd25525f6b688bbd6b2f53ce
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-02-23 22:50:55 -0700

    redirect after successfully submitting item

--- frontend/app/actions.ts
+++ frontend/app/actions.ts
@@ -1,9 +1,11 @@
 "use server";
+import { redirect } from "next/navigation";
 
 export const createListing = async (formData: FormData) => {
   const rawFormData = Object.fromEntries(formData.entries());
   //   parse and Send to API endpoint
   console.log(rawFormData);
+  redirect("/");
 };
 
 export const signOut = () => {};
