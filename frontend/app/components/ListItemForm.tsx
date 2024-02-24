baaac1cac2aaed36ee0e3a7ccb5cc14cfd543813
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-02-23 22:06:07 -0700

    refactor form templates

--- frontend/app/components/ListItemForm.tsx
+++ frontend/app/components/ListItemForm.tsx
@@ -1,4 +1,3 @@
-// import React, { useRef } from "react";
 import ListingDetails from "./listItemFormComponents/ListingDetails";
 import ListingMedia from "./listItemFormComponents/ListingMedia";
 import ListingLocation from "./listItemFormComponents/ListingLocation";
@@ -7,20 +6,14 @@ import SubmitButton from "./SubmitButton";
 import { createListing } from "../actions";
 import { ListItemProvider } from "../context/ListItemContext";
 export default function ListItemForm() {
-  // const ref = useRef<HTMLFormElement>(null);
   return (
-    <form
-      // ref={ref}
-      className="flex flex-col gap-4 text-brand"
-      action={createListing}
-      // ref.current?.reset();
-    >
+    <form className="flex flex-col gap-4 text-brand" action={createListing}>
       <ListItemProvider>
-        <ListingDetails></ListingDetails>
-        <ListingMedia></ListingMedia>
-        <ListingLocation></ListingLocation>
-        <ListingContactInformation></ListingContactInformation>
-        <SubmitButton></SubmitButton>
+        <ListingDetails />
+        <ListingMedia />
+        <ListingLocation />
+        <ListingContactInformation />
+        <SubmitButton />
       </ListItemProvider>
     </form>
   );
