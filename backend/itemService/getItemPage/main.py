2562c61568c0325eae732bd5e76782e1f315045c
joshuajipp <joshuajipp@gmail.com>
2024-03-04 14:56:53 -0700

    added capabilities for searching name and description

--- backend/itemService/getItemPage/main.py
+++ backend/itemService/getItemPage/main.py
@@ -20,22 +20,40 @@ def decimal_default(obj):
         return float(obj)
     raise TypeError
 
-def fetch_items_without_borrowerID_with_pagination(table_name, location, exclusiveStartKey, pageCount):
+def fetch_items_without_borrowerID_with_pagination(table_name, location, exclusiveStartKey, pageCount, search=None):
     table = get_dynamodb_table(table_name)
     fetched_items = []
     last_evaluated_key = exclusiveStartKey or None
+
+    # Start building the FilterExpression for attribute_not_exists condition
+    filter_expressions = ['attribute_not_exists(borrowerID)']
+    expression_attribute_values = {
+        ':value': location,
+    }
+    expression_attribute_names = {}
+
+    # If search parameter is provided, add conditions for itemName and itemDescription
+    if search:
+        # Dynamically generate expression attribute names to avoid conflicts
+        search_expression = "(contains(#itemName, :search) OR contains(#itemDescription, :search))"
+        filter_expressions.append(search_expression)
+        expression_attribute_values[':search'] = search
+        expression_attribute_names['#itemName'] = 'itemName'
+        expression_attribute_names['#itemDescription'] = 'itemDescription'
 
     while len(fetched_items) < pageCount:
         query_kwargs = {
             'IndexName': 'LocationTimestampIndex',
             'KeyConditionExpression': 'location = :value',
-            'ExpressionAttributeValues': {
-                ':value': location
-            },
-            'FilterExpression': 'attribute_not_exists(borrowerID)',
-            'Limit': pageCount, 
+            'ExpressionAttributeValues': expression_attribute_values,
+            'FilterExpression': " AND ".join(filter_expressions),
+            'Limit': pageCount,
             'ScanIndexForward': False
         }
+
+        # Add expression attribute names if search is used
+        if search:
+            query_kwargs['ExpressionAttributeNames'] = expression_attribute_names
 
         if last_evaluated_key:
             query_kwargs['ExclusiveStartKey'] = last_evaluated_key
@@ -48,7 +66,7 @@ def fetch_items_without_borrowerID_with_pagination(table_name, location, exclusi
             break  # No more items to fetch
     
     if len(fetched_items) > pageCount:
-        last_evaluated_key = fetched_items[pageCount - 1]
+        last_evaluated_key = fetched_items[pageCount - 1]['itemID']  # Ensure this is the correct key attribute
     # If we fetched more items than needed due to the final query call, trim the list
     return fetched_items[:pageCount], last_evaluated_key
 
