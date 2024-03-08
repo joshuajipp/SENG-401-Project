19c1f628a172858ca256abbd449c58b466eed8bf
SpencerVR1 <spencer.vanroessel@ucalgary.ca>
2024-03-08 15:55:44 -0700

    added avatar and dropdown to view user profile requesting item

--- frontend/app/components/Notification.tsx
+++ frontend/app/components/Notification.tsx
@@ -1,16 +1,39 @@
 import React from "react";
+import {
+  Avatar,
+  Dropdown,
+  DropdownItem,
+} from "flowbite-react";
 
 export default function Notification() {
   return (
-    <div className="bg-brand p-4 w-full mx-auto">
-      <p className="text-white text-lg mb-4">Joseph has Requested your Hammer!</p>
+    <div className="flex bg-brand p-4 w-full mx-auto">
       <div className="flex">
-        <button className="px-4 py-2 bg-red-500 hover:bg-red-600 text-white font-semibold rounded-md mr-4">
-          Decline
-        </button>
-        <button className="px-4 py-2 bg-green-500 hover:bg-green-600 text-white font-semibold rounded-md">
-          Accept
-        </button>
+        <Dropdown
+          inline
+          label={
+            <Avatar
+              alt="User settings"
+              img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+              rounded
+            />
+          }
+        >
+          <DropdownItem>View Profile</DropdownItem>
+        </Dropdown>
+      </div>
+      <div className="flex-col pl-4">
+        <p className="text-white text-lg mb-4">
+          Joseph has Requested your Hammer!
+        </p>
+        <div className="flex">
+          <button className="px-2 py-1 bg-red-500 hover:bg-red-600 text-white font-semibold rounded-md mr-4">
+            Decline
+          </button>
+          <button className="px-2 py-1 bg-green-500 hover:bg-green-600 text-white font-semibold rounded-md">
+            Accept
+          </button>
+        </div>
       </div>
     </div>
   );
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,10 @@
 import RequestRentalForm from "./components/RequestRentalForm";
+import Notification from "./components/Notification";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <RequestRentalForm></RequestRentalForm>
+      {/* <RequestRentalForm></RequestRentalForm> */}
+      <Notification></Notification>
     </main>
   );
 }
