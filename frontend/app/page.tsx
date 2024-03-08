83691eb1307bdee1661b9db6ebf25a758f4256ef
SpencerVR1 <spencer.vanroessel@ucalgary.ca>
2024-03-08 16:14:39 -0700

    reverted page.tsx to original

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,8 +3,7 @@ import Notification from "./components/Notification";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <RequestRentalForm></RequestRentalForm> */}
-      <Notification></Notification>
+      <RequestRentalForm></RequestRentalForm>
     </main>
   );
 }
