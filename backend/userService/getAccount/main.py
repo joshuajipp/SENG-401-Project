36a9109e64b225d1a124f299724c716a406726d7
HoussemZ4 <114112576+HoussemZ4@users.noreply.github.com>
2024-03-08 17:23:34 -0700

    get user fixes (#60)
    * get account by email
    
    * added gsi to mocked table for testing
    
    * error message for invalid id or email

--- backend/userService/getAccount/main.py
+++ backend/userService/getAccount/main.py
@@ -4,16 +4,40 @@ import boto3
 from decimal import Decimal
 
 
-
 def handler(event, context, table=None):
+    # pass in table for testing
     if table is None:
         dynamodb_resource = boto3.resource("dynamodb", region_name='ca-central-1')
         table = dynamodb_resource.Table("users-30144999")  
+    
     data = json.loads(event["body"])
-    userID = data["userID"]
+    
+    # check if request contains userID or email
+    if "userID" in data:
+        key_condition = Key("userID").eq(data["userID"])
+    elif "email" in data:
+        key_condition = Key("email").eq(data["email"])
+    else:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "message": "Missing userID or email in request"
+            })
+        }
+    
     try:
-        res = table.query(KeyConditionExpression=Key("userID").eq(userID))
+        res = table.query(KeyConditionExpression=key_condition)
         items = res["Items"]
+
+        if not items:
+            return {
+                "statusCode": 404,
+                "body": json.dumps({
+                    "message": "User not found"
+                })
+            }
+        
+        # convert decimal to floats for JSON serialization
         for item in items:
             for key, value in item.items():
                 if isinstance(value, Decimal):
@@ -30,12 +54,3 @@ def handler(event, context, table=None):
                     "message": str(e)
                 })     
         }
-
-# Put email in headers:
-# fetch('your-api-endpoint', {
-#     method: 'GET',
-#     headers: {
-#         'Content-Type': 'application/json',
-#         'email': email // Include email in headers
-#     }
-# })
