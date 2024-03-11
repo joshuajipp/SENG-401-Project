8e19bb10c0470c9c4e4017db35ee831a8f85641d
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-03-11 16:05:13 -0600

    Add ImageCarousel, ImageModal, and ImageViewer components

---
+++ frontend/app/listings/[itemID]/ImageViewer.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import ImageModal from "./ImageModal";
+import ImageCarousel from "./ImageCarousel";
+
+export default function ImageViewer() {
+  const LargeImageElement = () => {
+    return (
+      <div className=" size-[360px] relative">
+        <Image
+          alt="product image"
+          src="https://via.placeholder.com/443x590"
+          fill
+        />
+      </div>
+    );
+  };
+  const SmallImageElement = () => {
+    return (
+      <div className=" size-[120px] relative">
+        <Image
+          alt="product image"
+          src="https://via.placeholder.com/443x590"
+          fill
+        />
+      </div>
+    );
+  };
+  return (
+    <>
+      <div className=" bg-slate-200 gap-1 grid grid-cols-3 px-2">
+        <div className=" col-span-2 self-center flex justify-center items-center">
+          <LargeImageElement />
+        </div>
+        <div className="flex flex-col col-span-1 justify-around items-center">
+          <SmallImageElement />
+          <SmallImageElement />
+          <ImageModal>
+            <ImageCarousel></ImageCarousel>
+          </ImageModal>
+        </div>
+      </div>
+    </>
+  );
+}
--- frontend/app/listings/[itemID]/page.tsx
+++ frontend/app/listings/[itemID]/page.tsx
@@ -1,11 +1,14 @@
 import RequestRentalForm from "@/app/components/RequestRentalForm";
-import React from "react";
+import ImageViewer from "./ImageViewer";
 
 export default function page({ params }: { params: { itemID: string } }) {
   return (
-    <div>
-      Listing page for {params.itemID}
-      <RequestRentalForm></RequestRentalForm>
-    </div>
+    <>
+      <div className="flex flex-row justify-between gap-4">
+        {/* Listing page for {params.itemID} */}
+        <ImageViewer></ImageViewer>
+        <RequestRentalForm></RequestRentalForm>
+      </div>
+    </>
   );
 }
