d907737d9116411f4b9edcd3c1d54a8c830168c9
SpencerVR1 <spencer.vanroessel@ucalgary.ca>
2024-03-12 23:51:25 -0600

    called api for getting items with hardcoded userID

--- frontend/app/requested/page.tsx
+++ frontend/app/requested/page.tsx
@@ -1,5 +1,58 @@
+"use client";
+
 import React from "react";
+import { useState, useEffect } from "react";
+interface Item {
+  location: string;
+  lenderID: string;
+  timestamp: number;
+  condition: string;
+  category: string;
+  images: [];
+  itemID: string;
+  description: string;
+  imageHashes: [];
+  borrowRequests: string[];
+  itemName: string;
+}
 
 export default function page() {
-  return <div>page</div>;
+  const [requestedItems, setRequestedItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    // Fetch user items when component mounts
+    async function getLenderItems() {
+      const res = await fetch(
+        "https://iat6gyr54ckeyk532ukyqqqx6m0blqpr.lambda-url.ca-central-1.on.aws/",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            lenderID: "663b6585-2f65-4088-a346-9fe2ec9a9000",
+          },
+        }
+      );
+      if (res.status == 200) {
+        const itemObject = await res.json();
+        const items = itemObject["items"];
+        const filteredItems = items.filter(
+          (item: Item) => item.borrowRequests && item.borrowRequests.length > 0
+        );
+        setRequestedItems(filteredItems);
+      }
+    }
+    getLenderItems();
+  }, []);
+
+  return (
+    <div>
+      {requestedItems.length === 0 ? (
+        <p>Empty</p>
+      ) : (
+        <div>
+          <p>Not Empty</p>
+        </div>
+      )}
+    </div>
+  );
 }
