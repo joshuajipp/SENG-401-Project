e614fe497ef4e0ea75a840d78df099bd16bc6a48
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-03-13 11:18:40 -0600

    added client side check for user login on submit

--- frontend/app/components/SubmitButton.tsx
+++ frontend/app/components/SubmitButton.tsx
@@ -1,14 +1,29 @@
 "use client";
 import { Button } from "flowbite-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useFormStatus } from "react-dom";
 import { toast } from "react-toastify";
 import { functionThatReturnPromise } from "../utils/mockPromise";
+import { getSession } from "next-auth/react";
 function SubmitButton({ ...props }) {
   const pendingMessage = props.pending;
   const successMessage = props.success;
   const errorMessage = props.error;
   const { pending } = useFormStatus();
+  const [loggedIn, setLoggedIn] = useState(false);
+  useEffect(() => {
+    const fetchSession = async () => {
+      // getSession needs to be defined/imported
+      const session = await getSession();
+      console.log(session);
+      if (session) {
+        setLoggedIn(true);
+      }
+    };
+
+    fetchSession();
+  }, []);
+
   const notify = () =>
     toast.promise(functionThatReturnPromise, {
       pending: pendingMessage || "Listing is being uploaded...",
@@ -23,8 +38,13 @@ function SubmitButton({ ...props }) {
       className="  relative font-bold"
       aria-disabled={pending}
       onClick={notify}
+      disabled={!loggedIn}
     >
-      {pending ? "Processing..." : "Post Listing"}
+      {!loggedIn
+        ? "Please login to list item"
+        : pending
+        ? "Processing..."
+        : "Post Listing"}
     </Button>
   );
 }
