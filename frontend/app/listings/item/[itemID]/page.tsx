1a2bfe7c9fd1841a1d7a9771bfb13d99c118563f
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-03-13 22:18:54 -0600

    Remove commented out testItem object

--- frontend/app/listings/item/[itemID]/page.tsx
+++ frontend/app/listings/item/[itemID]/page.tsx
@@ -7,24 +7,6 @@ import { getItemFromID } from "@/app/actions";
 import { ItemsGetListI } from "@/app/interfaces/ListItemI";
 
 export default async function page({ params }: { params: { itemID: string } }) {
-  // const testItem = {
-  //   location: "Mockba",
-  //   lenderID: "037067cd-fff5-4b72-b45d-c7ae60723b18",
-  //   timestamp: 1710115788.8776865,
-  //   condition: "Used - Good",
-  //   category: "Legendary Items",
-  //   images: [
-  //     "https://res.cloudinary.com/dwviwvswa/image/upload/v1710115790/dxj8ofk8dpmvbgdqklkl.jpg",
-  //     "https://res.cloudinary.com/dwviwvswa/image/upload/v1710115791/oxu5jvwkfm6rnwnelzob.jpg",
-  //   ],
-  //   itemID: "c9562b7d-fd26-4c40-b9fe-9bd615b59605",
-  //   description: "Used for the glory of the country",
-  //   imageHashes: [
-  //     "422826b63f9fead82167c7282152ff22645d84309849e6f9c264312e0a859aaf",
-  //     "c183e27592b29bf34a869fb85ec14d585af45ab54c389d94d49d57a658334aac",
-  //   ],
-  //   itemName: "Hammer and Sickle",
-  // };
   const res = await getItemFromID(params.itemID);
   const testItem: ItemsGetListI = res.items;
   console.log(testItem);
