cd354f18837572021fce79179edf92c0454770a8
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-03-15 16:48:57 -0600

    Refactor ListingLocation component to fetch user location from server session

--- frontend/app/components/listItemFormComponents/ListingLocation.tsx
+++ frontend/app/components/listItemFormComponents/ListingLocation.tsx
@@ -3,30 +3,27 @@ import { FaLocationDot } from "react-icons/fa6";
 import ListingMapContainer from "./ListingMapContainer";
 import { Tooltip } from "flowbite-react";
 import ListItemFormTemplate from "./ListItemFormTemplate";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/utils/authOptions";
 
-export default function ListingLocation() {
-  const city = "Calgary";
-  const postalCode = "T3A 7V4";
-  const province = "AB";
-  const location = "Calgary, AB T3A 7V4";
-
+export default async function ListingLocation() {
+  const user = await getServerSession(authOptions);
+  // @ts-ignore
+  const location = user?.userData.location;
   return (
     <ListItemFormTemplate formNumber={3} formHeader={"Location"}>
       <div className="flex flex-row justify-between items-center place-items-center place-content-center ">
         <div className=" flex flex-col gap-4 text-sm">
-          <h2 className="text-brand font-bold ">
-            {city}, {postalCode}
-          </h2>
-          <div className="w-56 h-14 p-2.5 bg-stone-50 rounded border border-zinc-300 justify-start items-center gap-2.5 inline-flex">
-            <div className="size-10 flex justify-center items-center place-items-center bg-neutral-200 rounded-full">
-              <FaLocationDot className="text-black dark:text-white" />
+          <div className=" p-4 bg-stone-50 rounded border border-zinc-300 items-center gap-2 flex flex-row">
+            <div className="flex justify-center items-center place-items-center bg-neutral-200 size-10 rounded-full">
+              <FaLocationDot className="text-black dark:text-white" size={20} />
             </div>
-            <div>
-              <span className="text-neutral-500 text-xs font-normal">
+            <div className="text-neutral-500">
+              <span className="text-xs font-normal">
                 Your ad will be posted in
                 <br />
               </span>
-              <span className="text-neutral-500 text-sm font-bold">{city}</span>
+              <span className=" text-sm font-bold">{location}</span>
             </div>
           </div>
           <div className="flex flex-row place-items-center gap-2">
