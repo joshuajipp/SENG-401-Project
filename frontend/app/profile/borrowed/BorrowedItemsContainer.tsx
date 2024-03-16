332a6df930385bf87d2615f3a8681f35f4b7290b
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-03-15 19:15:23 -0600

    Refactor BorrowedItemsContainer and ItemRow components

--- frontend/app/profile/borrowed/BorrowedItemsContainer.tsx
+++ frontend/app/profile/borrowed/BorrowedItemsContainer.tsx
@@ -1,9 +1,20 @@
 import React from "react";
-import Notification from "@/app/components/Notification";
 import ItemRow from "./ItemRow";
 export default function BorrowedItemsContainer() {
+  const TableHeader = () => {
+    return (
+      <div className=" bg-[#634C9F] rounded shadow text-white flex flex-row gap-16 justify-evenly items-center p-4">
+        <div>{"Item Name"}</div>
+        <div>{"Lender Name"}</div>
+        <div>{"Start Date"}</div>
+        <div>{"End Date"}</div>
+        <div>{"Status"}</div>
+      </div>
+    );
+  };
   return (
-    <div>
+    <div className="gap-4 flex flex-col">
+      <TableHeader></TableHeader>
       <ItemRow></ItemRow>
     </div>
   );
--- frontend/app/profile/borrowed/ItemRow.tsx
+++ frontend/app/profile/borrowed/ItemRow.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 export default function ItemRow() {
   const startDate = new Date("2024-02-14").toLocaleDateString("en-US", {
     month: "long",
@@ -11,31 +12,31 @@ export default function ItemRow() {
     day: "numeric",
     year: "numeric",
   });
+  const lenderName = "Joseph Stalin";
+  const borrowedState = "Unreturned";
+  const itemName = "Wrench";
+  const toolImage = "https://via.placeholder.com/66x47";
+  const profileImage = "https://via.placeholder.com/55x48";
   return (
-    <div className=" bg-[#DDD8E9] rounded shadow flex flex-row gap-16 items-center p-4">
-      <div className="flex flex-row gap-2 items-center">
-        <Image
-          alt="Tool Image"
-          src="https://via.placeholder.com/66x47"
-          width={66}
-          height={47}
-        />
-        <div className="">Wrench</div>
-      </div>
-      <div className="flex flex-row gap-2 items-center">
+    <button className=" bg-[#DDD8E9] rounded transition duration-300 ease-in-out transform hover:scale-[1.02] shadow flex flex-row gap-16 items-center justify-evenly p-4">
+      <Link href="/" className="flex flex-row gap-2 items-center">
+        <Image alt="Tool Image" src={toolImage} width={66} height={47} />
+        <div>{itemName}</div>
+      </Link>
+      <Link href="/" className="flex flex-row gap-2 items-center">
         <div className=" size-12 relative">
           <Image
             alt="Profile Image"
-            src="https://via.placeholder.com/55x48"
+            src={profileImage}
             className="rounded-full"
             fill
           />
         </div>
-        <div className="">Joseph Stalin</div>
-      </div>
-      <div className="">{startDate}</div>
-      <div className="">{endDate}</div>
-      <div className=" text-red-500">Unreturned</div>
-    </div>
+        <div>{lenderName}</div>
+      </Link>
+      <div>{startDate}</div>
+      <div>{endDate}</div>
+      <div className=" text-red-500">{borrowedState}</div>
+    </button>
   );
 }
