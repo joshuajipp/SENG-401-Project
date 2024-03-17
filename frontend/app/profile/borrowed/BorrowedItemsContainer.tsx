d74d12171d77578dc873f9d5d762ad295d4ab16d
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2024-03-17 18:04:15 +0000

    Refactor BorrowedItemsContainer and ItemRow components

--- frontend/app/profile/borrowed/BorrowedItemsContainer.tsx
+++ frontend/app/profile/borrowed/BorrowedItemsContainer.tsx
@@ -2,20 +2,34 @@ import React from "react";
 import ItemRow from "./ItemRow";
 export default function BorrowedItemsContainer() {
   const TableHeader = () => {
+    const tableHeaders = [
+      "Item Name",
+      "Lender Name",
+      "Start Date",
+      "End Date",
+      "Status",
+    ];
     return (
-      <div className=" bg-[#634C9F] rounded shadow text-white flex flex-row gap-16 justify-evenly items-center p-4">
-        <div>{"Item Name"}</div>
-        <div>{"Lender Name"}</div>
-        <div>{"Start Date"}</div>
-        <div>{"End Date"}</div>
-        <div>{"Status"}</div>
-      </div>
+      <thead className=" bg-[#634C9F] rounded shadow text-white ">
+        <tr>
+          {tableHeaders.map((header, index) => {
+            return (
+              <th
+                key={index}
+                // className="bg-[#634C9F] text-white p-4"
+              >
+                {header}
+              </th>
+            );
+          })}
+        </tr>
+      </thead>
     );
   };
   return (
-    <div className="gap-4 flex flex-col">
+    <table className="table-auto">
       <TableHeader></TableHeader>
       <ItemRow></ItemRow>
-    </div>
+    </table>
   );
 }
--- frontend/app/profile/borrowed/ItemRow.tsx
+++ frontend/app/profile/borrowed/ItemRow.tsx
@@ -18,25 +18,37 @@ export default function ItemRow() {
   const toolImage = "https://via.placeholder.com/66x47";
   const profileImage = "https://via.placeholder.com/55x48";
   return (
-    <button className=" bg-[#DDD8E9] rounded transition duration-300 ease-in-out transform hover:scale-[1.02] shadow flex flex-row gap-16 items-center justify-evenly p-4">
-      <Link href="/" className="flex flex-row gap-2 items-center">
-        <Image alt="Tool Image" src={toolImage} width={66} height={47} />
-        <div>{itemName}</div>
-      </Link>
-      <Link href="/" className="flex flex-row gap-2 items-center">
-        <div className=" size-12 relative">
-          <Image
-            alt="Profile Image"
-            src={profileImage}
-            className="rounded-full"
-            fill
-          />
-        </div>
-        <div>{lenderName}</div>
-      </Link>
-      <div>{startDate}</div>
-      <div>{endDate}</div>
-      <div className=" text-red-500">{borrowedState}</div>
-    </button>
+    <tbody>
+      <tr className=" bg-[#DDD8E9] rounded transition duration-300 ease-in-out transform hover:scale-[1.02] shadow">
+        <td className="p-4">
+          <Link href="/" className="flex flex-row gap-2 items-center">
+            <Image alt="Tool Image" src={toolImage} width={66} height={47} />
+            <div>{itemName}</div>
+          </Link>
+        </td>
+        <td className="p-4">
+          <Link href="/" className="flex flex-row gap-2 items-center">
+            <div className=" size-12 relative">
+              <Image
+                alt="Profile Image"
+                src={profileImage}
+                className="rounded-full"
+                fill
+                />
+            </div>
+            <div>{lenderName}</div>
+                </Link>
+        </td>
+        <td className="p-4">
+          <div>{startDate}</div>
+        </td>
+        <td className="p-4">
+          <div>{endDate}</div>
+        </td>
+        <td className="p-4">
+          <div className=" text-red-500">{borrowedState}</div>
+        </td>
+      </tr>
+    </tbody>
   );
 }
