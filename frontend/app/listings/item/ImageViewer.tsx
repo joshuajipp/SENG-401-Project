95f994ec2fa8e2054cbfb69789a0fb040bb683d4
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-03-18 22:19:23 -0600

    Fix conditional rendering bug and update delete confirmation message

--- frontend/app/listings/item/ImageViewer.tsx
+++ frontend/app/listings/item/ImageViewer.tsx
@@ -28,7 +28,7 @@ export default function ImageViewer({ images }: { images: string[] }) {
   return (
     <>
       <div className=" bg-white rounded-lg dark:bg-slate-800 gap-1 grid grid-cols-3 px-2">
-        {images.length > 0 ? (
+        {images?.length > 0 ? (
           <>
             <div className=" col-span-2 self-center flex justify-center items-center">
               <LargeImageElement imageLink={images[0]} />
--- frontend/app/listings/item/[itemID]/DeleteItemModal.tsx
+++ frontend/app/listings/item/[itemID]/DeleteItemModal.tsx
@@ -43,7 +43,8 @@ export default function DeleteItemModal({ item }: { item: ItemsGetListI }) {
           <div className="text-center">
             <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
             <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-              Are you sure you want to delete your listing?
+              Are you sure you want to delete your listing? This action cannot
+              be undone.
             </h3>
             <div className="flex justify-center gap-4">
               <Button
