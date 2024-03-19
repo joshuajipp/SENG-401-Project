592565f8a32c4507387dd93e0d206ae9485c6e43
Harris Hasnain <harris.hasnain@ucalgary.ca>
2024-03-18 21:22:12 -0600

    added dark mode support for footer

--- frontend/app/components/layouts/Footer.tsx
+++ frontend/app/components/layouts/Footer.tsx
@@ -8,13 +8,13 @@ import { FaYoutube } from "react-icons/fa";
 export default function Footer() {
 
   return (
-    <div className="bg-brand h-52 flex flex-col justify-around items-center">
+    <div className="bg-brand text-black dark:text-white h-52 flex flex-col justify-around items-center">
       <div className="h-32 w-full flex justify-around items-center">
-        <div className="border-r border-white text-2xl flex-1 w-full text-center h-full"><b><u>Support</u></b><br></br>
+        <div className="border-r border-black dark:border-white text-2xl flex-1 w-full text-center h-full"><b><u>Support</u></b><br></br>
           <span className="hover:cursor-pointer hover:underline">toolshedsupport@gmail.com</span><br></br>
           <span className="hover:cursor-pointer hover:underline">+ 1 (587) 889-1865</span>
         </div>
-        <div className="border-r border-white text-2xl flex-1 w-full text-center h-full"><b><u>Quick Links</u></b><br></br>
+        <div className="border-r border-black dark:border-white text-2xl flex-1 w-full text-center h-full"><b><u>Quick Links</u></b><br></br>
           <span className="hover:cursor-pointer hover:underline">Privacy Policy</span><br></br>
           <span className="hover:cursor-pointer hover:underline">Terms of Use</span><br></br>
           <span className="hover:cursor-pointer hover:underline">FAQ</span>
@@ -38,7 +38,7 @@ export default function Footer() {
           </div>
         </div>
       </div>
-      <div className = "h-14 border-t border-white w-5/6 flex justify-center items-center">Copyright 2024 © ToolShed Inc. All rights reserved.</div>
+      <div className = "h-14 border-t border-black dark:border-white w-5/6 flex justify-center items-center">Copyright 2024 © ToolShed Inc. All rights reserved.</div>
     </div>
 
   );
