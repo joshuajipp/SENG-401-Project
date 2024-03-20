78b7c15b897909d1d5e2b7b6bf4a50215b0c9fe4
SpencerVR1 <spencer.vanroessel@ucalgary.ca>
2024-03-20 08:11:21 -0600

    added cancelRequest api call to actions.ts

--- frontend/app/actions.ts
+++ frontend/app/actions.ts
@@ -415,3 +415,27 @@ export const searchItemsRedirect = async (formData: FormData) => {
   }
   redirect(`/listings${searchQuery}`);
 };
+
+export const cancelRequest = async (itemID: string, borrowerID: string) => {
+  const response = await fetch(
+    "https://kwvu2ae5lllfz77k5znkrvnrii0brzuf.lambda-url.ca-central-1.on.aws/",
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        itemID: itemID,
+        borrowerID: borrowerID,
+      }),
+    }
+  );
+
+  if (response.status !== 200) {
+    const errorMessage =
+      "Failed to decline request. Status code: " + response.status;
+    console.error(errorMessage);
+    return errorMessage;
+  }
+  return response;
+};
