a19f101c967f8a2a16101f46298a285c45eb4236
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-03-22 19:09:36 -0600

    Add ListingRow component and refactor ListingsContainer

--- frontend/app/listings/components/ListingsContainer.tsx
+++ frontend/app/listings/components/ListingsContainer.tsx
@@ -1,11 +1,8 @@
 import { getItemPage } from "@/app/actions";
 import { ItemsGetListI } from "@/app/interfaces/ListItemI";
-import React from "react";
-import SortListingsButton from "./SortListingsButton";
-import Link from "next/link";
-import Image from "next/image";
-import { FaRegHeart } from "react-icons/fa";
 import { SearchParamsI } from "@/app/interfaces/SearchParamsI";
+import SortListingsButton from "./SortListingsButton";
+import ListingRow from "./ListingRow";
 
 export default async function ListingsContainer({
   searchParams,
@@ -29,33 +26,8 @@ export default async function ListingsContainer({
         <SortListingsButton />
       </div>
       <ul className="flex flex-col gap-1 ">
-        {listings.map((listing) => (
-          <li
-            key={listing.itemName}
-            className=" shadow rounded-lg border border-brand flex flex-row gap-4 p-4"
-          >
-            <Link href={`/listings/item/${listing.itemID}`}>
-              <Image
-                width={120}
-                height={120}
-                src={listing.images[0] || "/missingImage.jpg"}
-                alt="listing Image"
-                className="rounded"
-              />
-            </Link>
-
-            <div className=" flex flex-col gap-2">
-              <Link href={`/listings/item/${listing.itemID}`}>
-                <h1 className=" text-xl font-semibold">{listing.itemName}</h1>
-              </Link>
-
-              <h2 className=" text-sm font-bold">{listing.location}</h2>
-              <h3 className=" text-gray-600 text-xs font-normal">
-                {listing.description}
-              </h3>
-            </div>
-            <FaRegHeart className=" size-8 hover:scale-110 transition duration-300 ease-in-out cursor-pointer" />
-          </li>
+        {listings.map((listing, index) => (
+          <ListingRow key={index} listing={listing} />
         ))}
       </ul>
     </ul>
