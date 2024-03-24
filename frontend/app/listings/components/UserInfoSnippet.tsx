a2928a07073fe12bec98e13d597955c175a95222
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-03-24 16:40:14 -0600

    Add UserInfoSnippet component to RentalRequestComponent

---
+++ frontend/app/listings/components/UserInfoSnippet.tsx
@@ -0,0 +1,33 @@
+import { getUserByID } from "@/app/actions";
+import { UserI } from "@/app/interfaces/UserI";
+import React from "react";
+import { FaUserCircle } from "react-icons/fa";
+import { Rating, RatingStar } from "flowbite-react";
+import Link from "next/link";
+
+export default async function UserInfoSnippet({ userID }: { userID: string }) {
+  const lender: UserI = await getUserByID(userID);
+  console.log(lender);
+  const rating = lender.rating;
+  return (
+    <div className="flex flex-col gap-2">
+      <Link
+        href={`/profile/${lender.userID}`}
+        className=" flex flex-row gap-2 items-center"
+      >
+        <FaUserCircle size={40} />
+        <div className="text-xl">{lender.name}</div>
+      </Link>
+      <Rating>
+        {rating &&
+          Array.from({ length: rating }).map((_, i) => (
+            <RatingStar key={i} className="text-brand" />
+          ))}
+        {!rating &&
+          Array.from({ length: 5 }).map((_, i) => (
+            <RatingStar key={i} filled={false} />
+          ))}
+      </Rating>
+    </div>
+  );
+}
