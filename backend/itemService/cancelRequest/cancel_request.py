c81e7542dd94c27e9b497bfc74664f4f7154c93d
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2024-03-24 20:20:35 -0600

    removed string index

--- backend/itemService/cancelRequest/cancel_request.py
+++ backend/itemService/cancelRequest/cancel_request.py
@@ -48,7 +48,7 @@ def delete_borrow_request_array(table, itemID):
 def move_borrow_request_to_past_requests(table, itemID, data):
     """Move a borrow request to the pastRequests array in the DynamoDB table."""
     item = table.get_item(Key={'itemID': itemID})
-    past_requests = item.get('Item', {}).get('pastRequests', [])['request']
+    past_requests = item.get('Item', {}).get('pastRequests', [])
     print(f'Before {past_requests}')
     past_requests.append(data)
     print(f'After {past_requests}')
