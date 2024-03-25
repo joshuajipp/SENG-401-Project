22c9d0a5bbc010f48762bb2c27407f296b14519e
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2024-03-24 20:01:02 -0600

    test prints

--- backend/itemService/cancelRequest/cancel_request.py
+++ backend/itemService/cancelRequest/cancel_request.py
@@ -48,9 +48,10 @@ def delete_borrow_request_array(table, itemID):
 def move_borrow_request_to_past_requests(table, itemID, data):
     """Move a borrow request to the pastRequests array in the DynamoDB table."""
     item = table.get_item(Key={'itemID': itemID})
-    past_requests = item.get('Item', {}).get('pastRequests', [])
-
+    past_requests = item.get('Item', {}).get('pastRequests', [])['request']
+    print(f'Before {past_requests}')
     past_requests.append(data)
+    print(f'After {past_requests}')
 
     response = table.update_item(
         Key={
@@ -122,7 +123,9 @@ def handler(event, context):
                 responses.append(set_borrower_to_borrow_requests(table, itemID, requests))
 
             for cancelled_request in cancelled:
+                print(cancelled_request)
                 cancelled_request['request']['status'] = 'cancelled'
+                print(f'After {cancelled_request}')
                 responses.append(move_borrow_request_to_past_requests(table, itemID, cancelled_request))
                 
             return {
