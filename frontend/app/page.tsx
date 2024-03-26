752e28f0949938d9a7b3a03e4bac465bf614e2d2
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2024-03-25 23:17:51 -0600

    Add GetLocationComponent to Home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,11 @@
+import GetLocationComponent from "./components/GetLocationComponent";
 import HomePage from "./components/HomePage";
 
 export default async function Home() {
   return (
     <main>
       <HomePage></HomePage>
+      <GetLocationComponent />
     </main>
   );
 }
