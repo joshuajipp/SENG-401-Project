7525d9c9b8875329af01c069bdb9627dfdae41a9
Joshua Jipp <102313118+joshuajipp@users.noreply.github.com>
2024-03-27 11:03:26 -0600

    Update main.py

--- backend/itemService/requestItem/main.py
+++ backend/itemService/requestItem/main.py
@@ -43,6 +43,19 @@ def update_borrow_requests(table, itemID, data):
 
 def handler(event, context):
     try:
+        header = event["headers"]
+        if os.environ.get('ENV') != 'testing':
+            req = requests.get(f'https://www.googleapis.com/oauth2/v1/userinfo?access_token={header["accesstoken"]}',
+                        headers={
+                            "Authorization": f"Bearer {header['accesstoken']}",
+                            "Accept": "application/json"
+                        })
+        
+            if req.status_code != 200:
+                return {
+                "statusCode": 401,
+                "body": json.dumps({"message": "Invalid user"})
+                }
         table_name = 'items-30144999'
         table = get_dynamodb_table(table_name)
         body = parse_event_body(event["body"])
